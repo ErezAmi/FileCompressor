3ff686182c02dfc05737a37041515afa55a6184a
ErezAmi <erezamif@gmail.com>
2024-04-26 18:13:26 +0300

    ReadFile added

--- Compressor/main.cpp
+++ Compressor/main.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <String>
 #include <Windows.h>
+#include <vector>
 
 using namespace std;
 
@@ -21,6 +22,16 @@ int wmain(int argc, wchar_t** argv) // the number (argc) of parameters and the p
 		return 1;
 	}
 
-
+	DWORD size = GetFileSize(handle_to_file, NULL);//CHANGE LATER TO DEAL WITH LARGER FILES
+	vector<uint8_t> file_data;
+	file_data.resize(size);
+	DWORD number_of_bytes_read = 0;
+	bool result = ReadFile(handle_to_file, &file_data[0], size, &number_of_bytes_read, NULL);
+	if (!result)
+	{
+		printf("Failed to read file");
+		return 1;
+	}
+	printf("File size: %d\nNumber of bytes read: %d", size, number_of_bytes_read);
 	return 0;
 }
