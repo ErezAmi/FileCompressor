250d436968680cb9ec3ba567b4d4dce66809738f
ErezAmi <erezamif@gmail.com>
2024-04-26 17:53:27 +0300

    get path from cmd

--- Compressor/main.cpp
+++ Compressor/main.cpp
@@ -4,11 +4,15 @@
 
 using namespace std;
 
-int main(int argc, char** argv) // the number (argc) of parameters and the parameters (argv)
+int wmain(int argc, wchar_t** argv) // the number (argc) of parameters and the parameters (argv)
 {
-	printf("Number of parameters: %d",argc);
+	if (argc < 2)
+	{
+		printf("no parameters sent");
+		return 1;
+	}
 
-	wstring file_path = L"C:\\Users\\user\\OneDrive\\Desktop\\LPR-Check.txt";
+	wstring file_path = argv[1];
 
 	HANDLE handle_to_file = CreateFileW(file_path.c_str(), GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
 	if (handle_to_file == INVALID_HANDLE_VALUE)
@@ -16,10 +20,7 @@ int main(int argc, char** argv) // the number (argc) of parameters and the param
 		printf("\nFailed to open file");
 		return 1;
 	}
-	else
-	{
-		printf("\nall is well");
-	}
+
 
 	return 0;
 }
