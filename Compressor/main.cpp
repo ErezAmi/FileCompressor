0f8e5f9a54f1ed72445aa108291974c298282e87
ErezAmi <erezamif@gmail.com>
2024-04-26 13:48:45 +0300

    add stuff to .gitignor and make sure code can run through cmd

--- Compressor/main.cpp
+++ Compressor/main.cpp
@@ -1,6 +1,7 @@
+#include <stdio.h>
 
-int main()
+int main(int argc, char** argv) // the number (argc) of parameters and the parameters (argv)
 {
-
+	printf("Number of parameters: %d",argc);
 	return 0;
 }
